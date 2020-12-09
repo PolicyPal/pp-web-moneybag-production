53c4d9dedba0f6cbb98436448787303948006bc4
Wilson <wilson@policypal.com>
2020-12-09 14:57:26 +0800

    (chore) - Added copy nickname

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d118a64cd67dbac804b2";
+/******/ 	var hotCurrentHash = "4b6d243efef7d584db96";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
