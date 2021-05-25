69ff3ab49098e7b2e747beac5fd0a1fb3a83d716
Wilson <wilson@policypal.com>
2021-05-25 14:32:59 +0800

    (chore) - Update gameID to cater > 3

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "03f249cf723c05221a25";
+/******/ 	var hotCurrentHash = "e6f996de4dd5b15ff4de";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
